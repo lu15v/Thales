2da86c1ca138c2bf759119535806048bc65adda6
Fernando Gomez <gomezhyuuga@gmail.com>
2016-11-21 03:06:11 -0600

    Pin mapping

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,29 +1,44 @@
 var express = require('express');
 var router  = express.Router();
 
+var Promise = require('promise');
+
+var five  = require('johnny-five');
+var raspi = require('raspi-io');
+var board = new five.Board({
+    io: new raspi(),
+    repl: false,
+    debug: false
+});
+
+var board-ready = new Promise(function(resolve, reject) {
+  board.on('ready', function() {
+    resolve(five, board);
+  });
+});
+
+board-ready.then(function(five, board) {
+  console.log('Board ready');
+  var led = new five.Led(2);
+  led.on();
+});
+
 var SENSOR_MAP = {
   pre: {
-  'BP': false,
-  'BO': false,
-  'E1': false,
-  'E2': false,
-  'A1': false,
-  'A2': false
+    'BP': { active: false, pin: 0 },
+    'BO': { active: false, pin: 2 },
+    'E1': { active: false, pin: 3 },
+    'E2': { active: false, pin: 4 },
+    'A1': { active: false, pin: 5 },
+    'A2': { active: false, pin: 6 }
   },
   pos: {
-  'BP': false,
-  'BO': false,
-  'E1': false,
-  'A1': false,
-  'A2': false
-  }
-}
-
-var Sensor = function(type, time) {
-  return {
-    sensor: type,
-    time: time
-  }
+    'BP': { active: false, pin: 21 },
+    'BO': { active: false, pin: 22 },
+    'E1': { active: false, pin: 23 },
+    'A1': { active: false, pin: 25 },
+    'A2': { active: false, pin: 27 }
+  },
 }
 
 /* GET home page. */
