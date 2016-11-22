96851ac32dd870a61031f2d1be2c1bfac4c8b091
Fernando Gomez <gomezhyuuga@gmail.com>
2016-11-21 18:45:37 -0600

    Polling status of the relays

--- app/public/javascripts/thales.js
+++ app/public/javascripts/thales.js
@@ -18,16 +18,30 @@ var SWITCHES = {
   }
 };
 
-app.controller('SwitchController', function SwitchController($scope, $http) {
+var POLLING_INTERVAL = 5000;
+var POLLING;
+
+app.controller('SwitchController', function SwitchController($scope, $http, $interval) {
   $scope.switches = SWITCHES;
+
+  $scope.updateStatus = function () {
+    console.log(new Date() + ': Updating status...');
+    $http.get('/status')
+      .then(function(res) {
+        console.log(res);
+        $scope.switches = res.data;
+      });
+  }
+
+  POLLING = $interval($scope.updateStatus, POLLING_INTERVAL);
 
   $scope.toggle = function(clasif, sw) {
     var val = SWITCHES[clasif][sw];
     console.log('toggling ' + clasif + ': ' + sw + ' => ' + val);
 
     $http.post('/toggle', {
-        sensor: sw,
-        clasif: clasif
+      sensor: sw,
+      clasif: clasif
     }).then(function(response) {
       console.log(response.data);
     });
